dd05683de6e12262e289c5d981101898a893ee5d
cisuarez <117554825+cisuarez@users.noreply.github.com>
2023-08-17 18:19:58 -0500

    Update Eventos.java

--- src/Modelo/Eventos.java
+++ src/Modelo/Eventos.java
@@ -3,31 +3,35 @@ package Modelo;
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 
-public class Eventos {
 
-    public void textKeyPress (KeyEvent e) {
+
+public abstract class Evento {
+    public abstract void manejarEvento(KeyEvent e, JTextField textField);}
+
+class EventoTexto extends Evento {
+    @Override
+    public void manejarEvento(KeyEvent e, JTextField textField) {
         char caracter = e.getKeyChar();
         if ((caracter < 'a' || caracter > 'z') && (caracter < 'A' || caracter > 'Z')
-            && (caracter != (char) KeyEvent.VK_BACK_SPACE) && (caracter != (char) KeyEvent.VK_BACK_SPACE)) {
+                && (caracter != (char) KeyEvent.VK_BACK_SPACE) && (caracter != (char) KeyEvent.VK_BACK_SPACE)) {
+            e.consume();}}
+}
 
-            e.consume();
-        }
-    }
-
-    public void numberKeyPress (KeyEvent e) {
+class EventoNumerico extends Evento {
+    @Override
+    public void manejarEvento(KeyEvent e, JTextField textField) {
         char caracter = e.getKeyChar();
         if ((caracter < '0' || caracter > '9') && (caracter != (char) KeyEvent.VK_BACK_SPACE)) {
-            e.consume();
-        }
-    }
+            e.consume();}}
+}
 
-    public void numberDecimalKeyPress(KeyEvent e, JTextField textField) {
+class EventoDecimal extends Evento {
+    @Override
+    public void manejarEvento(KeyEvent e, JTextField textField) {
         char caracter = e.getKeyChar();
         if ((caracter < '0' || caracter > '9') && textField.getText().contains(".") && (caracter != (char) KeyEvent.VK_BACK_SPACE)) {
             e.consume();
         } else if ((caracter < '0' || caracter > '9') && (caracter != '.') && (caracter != (char) KeyEvent.VK_BACK_SPACE)) {
-            e.consume();
-        }
-    }
-
+            e.consume();}}
 }
+
